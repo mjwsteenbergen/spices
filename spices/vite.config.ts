9525f154f5a6d9b113894db83595d12f6e567807
Martijn Steenbergen <m.uses+git@outlook.com>
2023-07-31 15:36:49 +0000

    move spices into folder

---
+++ spices/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import react from '@vitejs/plugin-react';
+import { join } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/taskist",
+  server: {
+    fs: {
+      allow: [
+        // search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+        // your custom rules
+        join(searchForWorkspaceRoot(process.cwd()), ".."),
+      ],
+    },
+  },
+})
